313e2e8dcbf4d985517aeb6ffd38cb02a7864b69
deeko2000 <dengli300@caledonian.ac.uk>
2025-05-20 20:40:59 +0100

    index changes and failed js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function greet() {
-  alert("Welcome to leagueformat.com!");
-}
+const dropdown1 = document.getElementById('btnContinent');
+const dropdown2 = document.getElementById('btnCountry');
+const dropdown3 = document.getElementById('btnLeague');
+
+dropdown1.addEventListener('change', function () {
+      if (this.value) {
+        dropdown2.disabled = false;
+      } else {
+        dropdown2.disabled = true;
+
+      }
+    });
+
+    dropdown2.addEventListener('change', function () {
+      if (this.value) {
+        dropdown2.disabled = false;
+      } else {
+        dropdown3.disabled = true;
+
+      }
+    });
