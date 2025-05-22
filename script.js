0eae9e8b333d9389ff444056d18eb82d2b46617e
deeko- <declanenglish72@gmail.com>
2025-05-22 11:58:16 +0100

    test on script

--- script.js
+++ script.js
@@ -2,20 +2,5 @@ const dropdown1 = document.getElementById('btnContinent');
 const dropdown2 = document.getElementById('btnCountry');
 const dropdown3 = document.getElementById('btnLeague');
 
-dropdown1.addEventListener('change', function () {
-      if (this.value) {
-        dropdown2.disabled = false;
-      } else {
-        dropdown2.disabled = true;
 
-      }
-    });
-
-    dropdown2.addEventListener('change', function () {
-      if (this.value) {
-        dropdown2.disabled = false;
-      } else {
-        dropdown3.disabled = true;
-
-      }
-    });
+    
