e4503710497e99d081637f9ddf0e8e3a0e3826b2
deeko2000 <dengli300@caledonian.ac.uk>
2025-05-23 11:00:43 +0100

    cards with selects and accompanying js

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-  document.addEventListener('DOMContentLoaded', function () {
-    const continentItems = document.querySelectorAll('#continentMenu .dropdown-item');
+ document.getElementById('continentSelect').addEventListener('change', function() {
+   document.getElementById('countrySelect').removeAttribute('disabled');
+  });
 
-         continentItems.forEach(item => {
-      item.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent default link behavior
+   document.getElementById('countrySelect').addEventListener('change', function() {
+   document.getElementById('leagueSelect').removeAttribute('disabled');
+  });
 
-        // Do any other action, e.g., enable League dropdown
-        document.getElementById('btnCountry').removeAttribute('disabled');
-      });
-    });
-     });
+// document.getElementById("continentSelect").addEventListener("select", myFunction);
+   
 
-     document.addEventListener('DOMContentLoaded', function () {
-    const countryItems = document.querySelectorAll('#countryMenu .dropdown-item');
+// function myFunction() {
+//   document.getElementById("openingPara").innerText = "You selected some text!";
+//   document.getElementById('countrySelect').removeAttribute('disabled');
+// }
+   
+  
+  
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   const continentItems = document.querySelectorAll('#continentMenu .dropdown-item');
 
-         countryItems.forEach(item => {
-      item.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent default link behavior
+  //        continentItems.forEach(item => {
+  //     item.addEventListener('click', function (e) {
+  //       e.preventDefault(); // Prevent default link behavior
 
-        // Do any other action, e.g., enable League dropdown
-        document.getElementById('btnLeague').removeAttribute('disabled');
-      });
-    });
-     });
+  //       // Do any other action, e.g., enable League dropdown
+  //       document.getElementById('btnCountry').removeAttribute('disabled');
+  //     });
+  //   });
+  //    });
+
+  //    document.addEventListener('DOMContentLoaded', function () {
+  //   const countryItems = document.querySelectorAll('#countryMenu .dropdown-item');
+
+  //        countryItems.forEach(item => {
+  //     item.addEventListener('click', function (e) {
+  //       e.preventDefault(); // Prevent default link behavior
+
+  //       // Do any other action, e.g., enable League dropdown
+  //       document.getElementById('btnLeague').removeAttribute('disabled');
+  //     });
+  //   });
+  //    });
