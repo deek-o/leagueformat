dae6cefd75d81143be5fc4cccf5100147329424f
deeko- <declanenglish72@gmail.com>
2025-06-02 13:32:40 +0100

    beginning to datafill

---
+++ datafill.js
@@ -0,0 +1,26 @@
+const data = {
+  'UK': {
+   'England': ["Premier League", "Championship"],
+   'Scotland': ["SPFL", "SPFL Championship"],
+  },
+  'Europe': {
+   'Spain': ["La Liga", "La Liga 2"],
+   'Germany': ["Bundesliga", "2. Bundesliga"],
+  },
+  'Asia': {
+    'Japan': ["J1 League", "J2 League"],
+    'South Korea': ["K League 1", "K League 2"],
+  },
+  'Africa': {
+    'Japan': ["J1 League", "J2 League"],
+    'South Korea': ["K League 1", "K League 2"],
+  },
+  'North America': {
+    'Japan': ["J1 League", "J2 League"],
+    'South Korea': ["K League 1", "K League 2"],
+  },
+  'South America': {
+    'Japan': ["J1 League", "J2 League"],
+    'South Korea': ["K League 1", "K League 2"],
+  },
+};
