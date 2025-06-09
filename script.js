6ec17b79bfbf31c1d918601f80b760582c303126
deeko- <declanenglish72@gmail.com>
2025-06-09 16:01:45 +0100

    Populating countries and resetting select menu

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ const countrySelect = document.getElementById("countrySelect");
 const leagueSelect = document.getElementById("leagueSelect");
 
  
-   document.getElementById('regionSelect').addEventListener('change', function() {
-   document.getElementById('countrySelect').removeAttribute('disabled');
+  regionSelect.addEventListener('change', function() {
+  countrySelect.innerHTML = '<option value="">Select Country</option>';
+  countrySelect.removeAttribute('disabled');
   const selectedRegion = this.value.trim();
   console.log(selectedRegion + " is the selected region!");
   const countries = Object.keys(data[selectedRegion]);
   console.log("Countries is: ", countries);
 
-  // countries.forEach((country) => {
-  //   console.log("Country is: " + country);
-  //   const option = document.createElement("option");
-  //   option.value = country;
-  //   option.textContent = country;
-  //   countrySelect.appendChild(option);
-  // });
+  countries.forEach((country) => {
+    console.log("Country is: " + country);
+    const option = document.createElement("option");
+    option.value = country;
+    option.textContent = country;
+    countrySelect.appendChild(option);
+  });
   });
 
    document.getElementById('countrySelect').addEventListener('change', function() {
