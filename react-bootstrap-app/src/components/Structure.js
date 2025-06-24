2cfe77b10dcc67e992cffc6d29d777ee5ba05025
deek-o <declanenglish72@gmail.com>
2025-06-24 21:19:01 +0100

    cards

--- react-bootstrap-app/src/components/Structure.js
+++ react-bootstrap-app/src/components/Structure.js
@@ -2,18 +2,22 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MyNavbar from './Navbar';
-//className="justify-content-md-center"
+import BasicCard from './Cards';
+
+
 function MyStructure() {
   return (
     <>
     <MyNavbar/>
     <body className="bg-dark text-white">
     <Container >
-      <p>OOF</p>
+      <p className="text-center pt-4 pb-5">Welcome to leagueformat.com! This site is a passion project designed to inform users 
+        about the various leagues from around the world and save any confusion on how the teams will be promoted, relegated or crowned champions!
+        This site is created using HTML, CSS and JavaScript with the react-bootstrap framework. Hope you enjoy!</p>
       <Row className="text-center">
-        <Col>1 of 1</Col>
-        <Col>1 of 1</Col>
-        <Col>1 of 1</Col>
+        <Col><BasicCard/></Col>
+        <Col><BasicCard/></Col>
+        <Col><BasicCard/></Col>
       </Row>
     </Container> 
     </body>
