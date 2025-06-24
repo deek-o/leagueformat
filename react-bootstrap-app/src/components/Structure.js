1657cedcedb9a6801b9a9a4746c50892103afcaa
deek-o <declanenglish72@gmail.com>
2025-06-24 21:09:46 +0100

    centered text

--- react-bootstrap-app/src/components/Structure.js
+++ react-bootstrap-app/src/components/Structure.js
@@ -2,14 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import MyNavbar from './Navbar';
-
+//className="justify-content-md-center"
 function MyStructure() {
   return (
     <>
     <MyNavbar/>
-    <body className="bg-dark">
+    <body className="bg-dark text-white">
     <Container >
-      <Row>
+      <p>OOF</p>
+      <Row className="text-center">
         <Col>1 of 1</Col>
         <Col>1 of 1</Col>
         <Col>1 of 1</Col>
