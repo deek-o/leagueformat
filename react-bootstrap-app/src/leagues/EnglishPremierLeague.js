6a6f9b67365a2c722f6c9e93f3e0feea33c2f6a1
deek-o <91667683+deek-o@users.noreply.github.com>
2025-09-09 16:11:11 +0000

    test table

--- react-bootstrap-app/src/leagues/EnglishPremierLeague.js
+++ react-bootstrap-app/src/leagues/EnglishPremierLeague.js
@@ -1,10 +1,13 @@
-// EnglishPremierLeague.js
+import BasicTable from "../components/Table"
 function EnglishPremierLeague() {
   return (
     <div>
       <h1>English Premier League</h1>
       <p>Details about the EPL go here…</p>
+      <BasicTable/>
     </div>
+
+    
   );
 }
 
