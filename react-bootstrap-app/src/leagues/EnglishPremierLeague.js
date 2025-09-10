71c35193e0dc606959f8e8ec9510dccf8f75b970
deek-o <declanenglish72@gmail.com>
2025-09-10 18:08:24 +0100

    .

--- react-bootstrap-app/src/leagues/EnglishPremierLeague.js
+++ react-bootstrap-app/src/leagues/EnglishPremierLeague.js
@@ -5,29 +5,6 @@ import BasicTable from "../components/Table";
 import { englishTeamData } from "../data/englishTeamStats";
 
 const teams = [englishTeamData];
-// const teams = [
-//   {
-//     badge: "/arsenal.png",
-//     club: "Arsenal",
-//     played: 38,
-//     points: 87,
-//     goalDifference: 55,
-//   },
-//   {
-//     badge: "/man-city.png",
-//     club: "Man City",
-//     played: 38,
-//     points: 89,
-//     goalDifference: 61,
-//   },
-//   {
-//     badge: "/chelsea.png",
-//     club: "Chelsea",
-//     played: 38,
-//     points: 74,
-//     goalDifference: 40,
-//   },
-// ];
 
 function EnglishPremierLeague() {
   return (
